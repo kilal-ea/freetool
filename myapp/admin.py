39f060231f1851a6bdc43d3c8b6fa7bdfe4ac554
kilal-ea <155766402+kilal-ea@users.noreply.github.com>
2026-03-07 21:37:03 +0000

    Register PageStatus model in admin site

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import BlockedIP, ContactMessage, ErrorLog, FileProcess, IPUsage, SystemMetric, ToolUsage, VisitEvent
+from .models import BlockedIP, ContactMessage, ErrorLog, FileProcess, IPUsage, SystemMetric, ToolUsage, VisitEvent , PageStatus
 
 
 admin.site.register(ToolUsage)
@@ -11,3 +11,4 @@ admin.site.register(BlockedIP)
 admin.site.register(IPUsage)
 admin.site.register(VisitEvent)
 admin.site.register(ContactMessage)
+admin.site.register(PageStatus)
